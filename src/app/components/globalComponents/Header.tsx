2495c6ed446bb7c06c72f034733f0747d788dc48
Dex Mills <dmills@instructors.2u.com>
2024-11-11 16:58:29 -0700

    Add header

---
+++ src/app/components/globalComponents/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-screen h-24 bg-gradient-to-r from-purple-950 via-red-500 to-pink-450 text-center">
+            Dex Mills
+        </div>
+    );
+}
